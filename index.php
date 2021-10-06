6388b1632e1ebb88b04c4b4bdf8027cd456f060a
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2021-10-06 17:22:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
 <?php
-    header("link: <style.css>; rel=stylesheet");
-    header("Refresh: 5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+header("link: <style.css>; rel=stylesheet");
+header("Refresh: 5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 ?>
